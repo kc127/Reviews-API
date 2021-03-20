de7a4b410275c2e9d81d22e0c56be2ce37465cce
Kanchan Chauhan <kanchan827@gmail.com>
2021-03-19 19:04:14 -0700

    finished loading the data into the database

--- readcsv/index.js
+++ readcsv/index.js
@@ -6,25 +6,12 @@ const rl = readline.createInterface({
   input: fs.createReadStream('new.csv', {encoding: "utf8"}, {start: 1}),
   crlfDelay: Infinity
 });
-const fieldsName = [ 'id',
-'product_id',
-'rating',
-'date',
-'summary',
-'body',
-'recommend',
-'reported',
-'reviewer_name',
-'reviewer_email',
-'response',
-'helpfulness' ]
+
 const outStream = fs.createWriteStream('clean_new.csv', {encoding: "utf8"});
 //outStream.write('id' + '\n')
 const count = 0;
 
 rl.on('line', (line) => {
-
-
 
     const fields = line.split(',');
     const totalFields = 12;
@@ -45,7 +32,7 @@ rl.on('line', (line) => {
     /* date should be in formate "2019-06-23" */
 
     /* summary: 1 sentence, 60 chars, body: 50 - 1000 chars */
-    if(fields[4].length > 60)  {
+    if(fields[4] !== 'summary' && fields[4].length > 60)  {
       return;
     }
 
@@ -54,7 +41,6 @@ rl.on('line', (line) => {
     }
 
     console.log(fields);
-
     outStream.write(line + '\n');
 
 
