3303e4210e5777d89f59e1e35314a615abe75148
Kanchan Chauhan <kanchan827@gmail.com>
2021-03-26 19:21:53 -0700

    Update index.js

--- reviewsAPI/dbsql/index.js
+++ reviewsAPI/dbsql/index.js
@@ -1,19 +1,20 @@
 var mysql = require('mysql');
 
 const db = mysql.createPool({
-  host:'ec2-100-25-146-78.compute-1.amazonaws.com',
+  host:'ec2-3-90-82-232.compute-1.amazonaws.com',
   port: 3306,
-  user:'root',
+  user:'user1',
   password:'password',
   database:'retaildb',
-  waitForConnections: true,
-  connectionLimit:10,
+  waitForConnection: true,
+  connectionLimit:1000,
+  connectionTimeout: 60*60*1000,
   queueLimit: 0
 });
 
 db.getConnection((err) => {
   if(err){
-    console.log("err connecting to mysql");
+    console.log("err connecting to mysql",err);
     return;
   }
   console.log("successfully connected to mysql")
@@ -21,6 +22,7 @@ db.getConnection((err) => {
 
 module.exports = db;
 
+/* localhost connection */
 // var mysql = require('mysql');
 
 // const db = mysql.createConnection({
